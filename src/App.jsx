e13637a7ce43bc53e85944aae6a90c1d6c180efb
Valentina <vrudntiskaya@gmail.com>
2024-02-26 16:11:23 -0600

    Add a new component AddTodoForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 
+import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 function App() {
   return (
     <>
       <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </>
   )
 }
 
-export default App
+export default App;
