3269f6d80c5e9764a197092f29e562407ffa59c5
Valentina <vrudntiskaya@gmail.com>
2024-02-26 16:00:24 -0600

    Add a new component named ToDoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
 import './App.css';
 
-const todoList = [
-  {
-    title: 'Read pages 1 through 37 of textbook',
-    id: 1
-  },
-  {
-    title: 'Watch tutorial videos',
-    id: 2
-  },
-  {
-    title: 'Complete assignment',
-    id: 3
-  },
-];
+import TodoList from './TodoList';
 
 function App() {
   return (
     <>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(item => {
-          return (
-            <li key={item.id}>
-              {item.title}
-            </li>
-          );
-        })}
-      </ul>
+      <TodoList />
     </>
   )
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,30 @@
+const todoList = [
+    {
+        title: 'Read pages 1 through 37 of textbook',
+        id: 1
+    },
+    {
+        title: 'Watch tutorial videos',
+        id: 2
+    },
+    {
+        title: 'Complete assignment',
+        id: 3
+    },
+];
+
+function TodoList() {
+    return (
+        <ul>
+            {todoList.map(item => {
+                return (
+                    <li key={item.id}>
+                        {item.title}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+
+export default TodoList;
