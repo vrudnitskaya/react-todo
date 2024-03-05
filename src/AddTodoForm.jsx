c5e6969fd26951c454a0b2c7b2e167c324add585
Valentina <vrudntiskaya@gmail.com>
2024-03-04 21:08:04 -0600

    Handle Add Todo Form Submit

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,15 @@
 const AddTodoForm = () => {
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        const todoTitle = document.querySelector('#todoTitle').value;
+        console.log(todoTitle);
+        document.querySelector('#todoTitle').value = '';
+    };
+    
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
-            <input id="todoTitle" type="text" />
+            <input id="todoTitle" type="text" name="title"/>
             <button type="submit">Add</button>
         </form>
     );
