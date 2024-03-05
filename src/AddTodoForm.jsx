4b596da4cbda376aafb3d9c2b392904ef883078c
Valentina <vrudntiskaya@gmail.com>
2024-03-04 18:14:19 -0600

    Edit input tag

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -2,7 +2,7 @@ const AddTodoForm = () => {
     return (
         <form>
             <label htmlFor="todoTitle">Title</label>
-            <input id="todoTitle" type="text"></input>
+            <input id="todoTitle" type="text" />
             <button type="submit">Add</button>
         </form>
     );
