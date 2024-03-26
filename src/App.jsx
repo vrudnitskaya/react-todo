bc6be295be928afd2cc3df3d58490528c28b622d
Valentina <vrudntiskaya@gmail.com>
2024-03-26 13:06:32 -0500

    Step 1. Save Todo List in Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
-import { useState } from 'react';
 
 const App = () => {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')) || []);
   const addTodo = (newTodo) => {
     setTodoList([newTodo, ...todoList])
   };
+  
+  useEffect(()=> {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+  },[todoList]);
   
   return (
     <>
