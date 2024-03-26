bca06c8144f1403ea20d9b8452297205ffd31fe1
Valentina <vrudntiskaya@gmail.com>
2024-03-26 14:00:45 -0500

    Step 2-3 create a custom hook, check if a fragment is used

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import './App.css';
+import "./App.css";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
+
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')) || []);
+  
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
 
 const App = () => {
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')) || []);
+  const [todoList, setTodoList] = useSemiPersistentState();
   const addTodo = (newTodo) => {
-    setTodoList([newTodo, ...todoList])
+    setTodoList([newTodo, ...todoList]);
   };
-  
-  useEffect(()=> {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-  },[todoList]);
-  
+
   return (
     <>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList}/>
+      <AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </>
-  )
-}
+  );
+};
 
 export default App;
