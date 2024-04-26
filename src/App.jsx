cc373c0b1352d64e7ea4313337da19ec0ee72f03
Valentina <vrudntiskaya@gmail.com>
2024-04-25 22:33:16 -0500

    add async fetch data function, edit add and remove todo functions, add spinner

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,100 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 import AddTodoForm from "./AddTodoForm";
+import Spinner from "./Spinner";
 import TodoList from "./TodoList";
+
+const request = async (options, url) => {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`Error has ocurred: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 const App = () => {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  
-  useEffect(()=> {
-    new Promise((resolve) => 
-    setTimeout(() => resolve( { data: {todoList: JSON.parse(localStorage.getItem('savedTodoList')) || []}}), 
-      2000))
-      .then(result => {
-        setTodoList(result.data.todoList);
+
+  const _apiBase = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = () => {
+    setIsLoading(true);
+    request({
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
+      }
+    }, _apiBase)
+      .then(data => {
+        const todos = data.records.map((todo) => {
+          return {
+            id: todo.id,
+            title: todo.fields.title
+          }
+        });
+        setTodoList(todos);
         setIsLoading(false);
       })
-  }, []);
-  
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-    }
-  }, [todoList]);
-  
-  const addTodo = (newTodo) => {
-    setTodoList([newTodo, ...todoList]);
+  }
+
+  const addTodo = (todoTitle) => {
+    const addedTodo = {
+      fields: {
+        title: todoTitle,
+      },
+    };
+
+    request({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+      body: JSON.stringify(addedTodo),
+    }, _apiBase)
+      .then((data) => {
+        const postedTodo = {
+          id: data.id,
+          title: data.fields.title
+        }
+        setTodoList([...todoList, postedTodo]);
+      });
   };
-  
-  const removeTodo = (id) =>{
-    const editedTodoList = todoList.filter(todo => todo.id !== id);
-    setTodoList(editedTodoList);
+
+  const removeTodo = (id) => {
+    const removedId = {
+      id: id,
+    };
+
+    request({
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+      body: JSON.stringify(removedId),
+    }, `${_apiBase}\/${removedId.id}`)
+      .then(data => {
+        const editedTodoList = todoList.filter(todo => todo.id !== data.id);
+        setTodoList(editedTodoList);
+      });
   }
 
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? <p>Loading...</p> 
-                  : <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+      {isLoading ? <Spinner />
+                : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
       }
     </>
   );
