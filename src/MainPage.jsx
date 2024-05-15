f2d9161ae39a90dde8571b512dbf16967ec61e5f
Valentina <vrudntiskaya@gmail.com>
2024-05-15 13:20:52 -0500

    change some styles, adapt for mobile devices

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -5,8 +5,8 @@ import styles from './MainPage.module.css';
 const MainPage = () => {
     return (
         <div className={styles.mainPageWrapper}>
-            <h1>To-Do List App</h1><img src={img} alt="Todo list image"/>
-            
+            <h1>To-Do List App</h1>
+            <img src={img} alt="Todo list image"/>
             <Link to='/todos'><button>Let's start!</button></Link>
         </div>
     )
